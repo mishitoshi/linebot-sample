849150ca866e56bb34850674b65fdaf6d4700691
mishitokun <toshimishito000@gmail.com>
2019-09-26 19:12:55 +0900

    add linebot_controller

---
+++ app/controllers/linebot_controller.rb
@@ -0,0 +1,38 @@
+class LinebotController < ApplicationController
+  require 'line/bot' # gem 'line-bot-api'
+
+  # callbackアクションのCSRFトークン認証を無効
+  protect_from_forgery except: [:callback]
+
+  def client
+    @client ||= Line::Bot::Client.new do |config|
+      config.channel_id = ENV['LINE_CHANNEL_ID']
+      config.channel_secret = ENV['LINE_CHANNEL_SECRET']
+      config.channel_token = ENV['LINE_CHANNEL_TOKEN']
+    end
+  end
+
+  def callback
+    body = request.body.read
+
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    head :bad_request unless client.validate_signature(body, signature)
+
+    events = client.parse_events_from(body)
+    events.each do |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+          client.reply_message(event['replyToken'], message)
+        end
+      end
+    end
+
+    head :ok
+  end
+end
